0010dd5ecf63e778852b08d02964a74e07417124
Lily Chieng <lilychieng556@gmail.com>
2022-03-28 23:38:02 -0500

    readded folder

---
+++ my-app/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import React from 'react';
+import {useState} from 'react'
+
+function App() {
+
+  const [data, setData] = useState();
+  const [error, setError] = useState("");
+
+  return (
+    // Headers and Text
+    <div className="App">
+      <h1>Homework 6: Lily Chieng (LCC2483)</h1>
+      <header className="App-header">
+
+        <div class="flex-grid">
+          <div class="flex-col">
+            Your Input Request to Server
+          </div>
+
+          <div class="flex-col">
+            Your Output Request to Server
+          </div>
+        </div>
+
+        <div class="flex-grid">
+
+          {/* Input Field */}
+          <div class="flex-col">
+            <input id = "pw-box-pads" type="text" value={data} onChange={
+              (e) => {
+                setData(e.target.value);
+                fetch("/name/" + e.target.value)
+                  .then(response => response.json())
+                  .then(data => {setError(data.last_name)})
+                  .catch(error => {setError(error)})
+                }
+              }
+                />
+          </div>
+
+          {/* Output Field */}
+          <div class="flex-col">
+            <textarea id = "u-box-pads" type="text" value={error} />
+          </div>
+
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
