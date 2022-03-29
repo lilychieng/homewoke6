6a4cc1743d0cacf0c09676d81777e2614f327381
Lily Chieng <lilychieng556@gmail.com>
2022-03-29 17:27:00 -0500

    change server

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
                 setData(e.target.value);
                 // fetch("http://127.0.0.1:5000/name/" + e.target.value)
                 // fetch("http://localhost:5000/name/" + e.target.value)
-                  fetch("http://172.30.0.221:80/name/" + e.target.value)
+                  fetch("/name/<input>" + e.target.value)
                   .then(response => response.json())
                   .then(data => {setError(data.last_name)})
                   .catch(error => {setError(error)})
