c5c92717bbd74908458173f4a539abb1185eab13
ZulimaIke <55000556+ZulimaIke@users.noreply.github.com>
2022-03-29 16:28:27 -0500

    Things I included in my code..
    I did a pip install flask-restful too and added that to my requirements file.

--- backend.py
+++ backend.py
@@ -2,10 +2,16 @@ from flask import Flask, jsonify;
 from typing import Optional;
 # from flask_cors import CORS
 
+#from flask_restful import Api, Resource, reqparse
+#from api.apihandler import ApiHandler
+
 app = Flask(__name__, static_folder="/my-app/build", static_url_path="") 
 # app = Flask(__name__)
 
 # CORS(app)
+
+#api = Api(app)
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
@@ -20,5 +26,8 @@ def print_lastname(input: Optional[str]=None):
 
     return jsonify(last_name=output)
 
+#api.add_resource(ApiHandler, '/')
+
 if __name__ == '__main__':
     app.run(debug=True)
+    #app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
