96b075b443ea2ab8dc55e7cfad522ddc3075bd39
Lily Chieng <lilychieng556@gmail.com>
2022-03-29 17:34:04 -0500

    add cor

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
                 setData(e.target.value);
                 // fetch("http://127.0.0.1:5000/name/" + e.target.value)
                 // fetch("http://localhost:5000/name/" + e.target.value)
-                  fetch("/name/<input>" + e.target.value)
+                  fetch("/name/" + e.target.value)
                   .then(response => response.json())
                   .then(data => {setError(data.last_name)})
                   .catch(error => {setError(error)})
