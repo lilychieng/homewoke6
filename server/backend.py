8004230f925e677fe0464229cae2a45e36cbbcd9
Lily Chieng <lilychieng556@gmail.com>
2022-03-28 22:59:47 -0500

    redid new

--- server/backend.py
+++ server/backend.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
-from typing import Optional
-app = Flask(__name__)
+from flask import Flask, jsonify;
+from typing import Optional;
+from flask_cors import CORS
 
+app = Flask(__name__, static_folder="../my-app/build", static_url_path="") 
+# app = Flask(__name__)
+CORS(app)
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
 @app.route("/name/")
-@app.route("/name/<input>",methods=["GET"])
+@app.route("/name/<input>", methods=["GET"])
 def print_lastname(input: Optional[str]=None):
     if (input == "Lily"):
         output = "Chieng"
